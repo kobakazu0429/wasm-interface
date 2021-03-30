dc728b356e5c70365f9de6608c6a18d3a73025d6
kobakakzu0429 <kobakazu10@gmail.com>
2021-03-30 17:36:43 +0900

    add FSStream

---
+++ src/FSStream.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+import { StreamOps, Tty } from "./TTY";
+import { FSNode } from "./FSNode";
+
+export class FSStream {
+  constructor() {}
+
+  public node: FSNode;
+  public flags!: number;
+  public fd!: number;
+  public stream_ops!: StreamOps;
+  public path!: string;
+  public seekable!: boolean;
+  public position!: number;
+  public ungotten!: any[];
+  public error!: boolean;
+  public getdents!: null;
+  public tty: Tty;
+
+  public get object() {
+    return this.node;
+  }
+
+  public set object(val) {
+    this.node = val;
+  }
+
+  public get isRead() {
+    return (this.flags & 2097155) !== 1;
+  }
+
+  public get isWrite() {
+    return (this.flags & 2097155) !== 0;
+  }
+
+  public get isAppend() {
+    return this.flags & 1024;
+  }
+}
